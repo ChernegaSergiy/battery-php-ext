f6f3cead54b54db6a4c5e0e299f09dac181783df
Serhii Cherneha <chernegasergiy3@gmail.com>
2026-02-01 03:53:21 +0200

    refactor: Improve macOS battery implementation

--- mac_battery.c
+++ mac_battery.c
@@ -1,63 +1,110 @@
+/**
+ * mac_battery.c - macOS battery information implementation
+ *
+ * Uses IOKit Power Sources API
+ */
+
+#include "include/battery_common.h"
+
 #if defined(__APPLE__)
+
 #include <CoreFoundation/CoreFoundation.h>
 #include <IOKit/ps/IOPowerSources.h>
 #include <IOKit/ps/IOPSKeys.h>
-#endif
 
-int mac_battery_level(void)
+void mac_battery_get_details(zval *return_value)
 {
-#if defined(__APPLE__)
+    zval batteries;
+    array_init(&batteries);
+
+    int total_level = 0;
+    int battery_count = 0;
+    int is_charging = 0;
+
     CFTypeRef blob = IOPSCopyPowerSourcesInfo();
-    if (!blob) return -1;
-    CFArrayRef list = IOPSCopyPowerSourcesList(blob);
-    if (!list) {
+    if (blob) {
+        CFArrayRef list = IOPSCopyPowerSourcesList(blob);
+        if (list) {
+            long source_count = CFArrayGetCount(list);
+
+            for (int i = 0; i < source_count; i++) {
+                CFDictionaryRef ps = IOPSGetPowerSourceDescription(
+                    blob, CFArrayGetValueAtIndex(list, i));
+                if (!ps) {
+                    continue;
+                }
+
+                /* Filter for internal batteries only */
+                CFStringRef type = CFDictionaryGetValue(ps, CFSTR(kIOPSTypeKey));
+                if (type && CFStringCompare(type, CFSTR(kIOPSInternalBatteryType), 0)
+                    != kCFCompareEqualTo) {
+                    continue;
+                }
+
+                zval bat;
+                array_init(&bat);
+
+                /* Battery name */
+                CFStringRef name = CFDictionaryGetValue(ps, CFSTR(kIOPSNameKey));
+                if (name) {
+                    char buf[64];
+                    if (CFStringGetCString(name, buf, sizeof(buf), kCFStringEncodingUTF8)) {
+                        add_assoc_string(&bat, BATTERY_KEY_NAME, buf);
+                    }
+                }
+
+                /* Capacity level */
+                CFNumberRef cap = CFDictionaryGetValue(ps, CFSTR(kIOPSCurrentCapacityKey));
+                if (cap) {
+                    int level = -1;
+                    CFNumberGetValue(cap, kCFNumberIntType, &level);
+                    add_assoc_long(&bat, BATTERY_KEY_LEVEL, level);
+                    total_level += level;
+                    battery_count++;
+                } else {
+                    add_assoc_null(&bat, BATTERY_KEY_LEVEL);
+                }
+
+                /* Charging status */
+                CFStringRef state = CFDictionaryGetValue(ps, CFSTR(kIOPSPowerSourceStateKey));
+                if (state) {
+                    int charging = (CFStringCompare(state, CFSTR(kIOPSACPowerValue), 0)
+                                    == kCFCompareEqualTo) ? 1 : 0;
+                    add_assoc_bool(&bat, BATTERY_KEY_CHARGING, charging);
+                    add_assoc_string(&bat, BATTERY_KEY_STATUS,
+                        charging ? BATTERY_STATUS_CHARGING : BATTERY_STATUS_DISCHARGING);
+                    if (charging) {
+                        is_charging = 1;
+                    }
+                } else {
+                    add_assoc_null(&bat, BATTERY_KEY_CHARGING);
+                    add_assoc_null(&bat, BATTERY_KEY_STATUS);
+                }
+
+                add_next_index_zval(&batteries, &bat);
+            }
+            CFRelease(list);
+        }
         CFRelease(blob);
-        return -1;
     }
-    if (CFArrayGetCount(list) == 0) {
-        CFRelease(list);
-        CFRelease(blob);
-        return -1;
+
+    /* Add batteries array */
+    add_assoc_zval(return_value, BATTERY_KEY_BATTERIES, &batteries);
+
+    /* Add aggregated values */
+    if (battery_count > 0) {
+        BATTERY_ADD_INFO(return_value, total_level / battery_count, is_charging);
+    } else {
+        BATTERY_ADD_NULL_INFO(return_value);
     }
-    CFDictionaryRef ps = IOPSGetPowerSourceDescription(blob, CFArrayGetValueAtIndex(list, 0));
-    if (!ps) {
-        CFRelease(list);
-        CFRelease(blob);
-        return -1;
-    }
-    CFNumberRef num = CFDictionaryGetValue(ps, CFSTR(kIOPSCurrentCapacityKey));
-    int v = -1;
-    if (num) CFNumberGetValue(num, kCFNumberIntType, &v);
-    CFRelease(list);
-    CFRelease(blob);
-    return v;
-#else
-    (void)0;
-    return -1;
-#endif
 }
 
-int mac_battery_is_charging(void)
-{
-#if defined(__APPLE__)
-    CFTypeRef blob = IOPSCopyPowerSourcesInfo();
-    if (!blob) return -1;
-    CFArrayRef list = IOPSCopyPowerSourcesList(blob);
-    if (!list) { CFRelease(blob); return -1; }
-    if (CFArrayGetCount(list) == 0) { CFRelease(list); CFRelease(blob); return -1; }
-    CFDictionaryRef ps = IOPSGetPowerSourceDescription(blob, CFArrayGetValueAtIndex(list, 0));
-    if (!ps) { CFRelease(list); CFRelease(blob); return -1; }
-    CFStringRef state = CFDictionaryGetValue(ps, CFSTR(kIOPSPowerSourceStateKey));
-    int charging = -1;
-    if (state) {
-        if (CFStringCompare(state, CFSTR(kIOPSACPowerValue), 0) == kCFCompareEqualTo) charging = 1;
-        else charging = 0;
-    }
-    CFRelease(list);
-    CFRelease(blob);
-    return charging;
 #else
-    (void)0;
-    return -1;
-#endif
+
+void mac_battery_get_details(zval *return_value)
+{
+    BATTERY_INIT_EMPTY(return_value);
 }
+
+#endif
+
